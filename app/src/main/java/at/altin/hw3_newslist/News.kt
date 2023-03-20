72f4a863ad4d47ec09acc0766dcec7984b62464b
altink7 <kelmendialtin48@gmail.com>
2023-03-20 19:31:43 +0100

    data angepasst

--- app/src/main/java/at/altin/hw3_newslist/News.kt
+++ app/src/main/java/at/altin/hw3_newslist/News.kt
@@ -59,10 +59,11 @@ fun parseXmlNews(result: String): List<NewsItem> {
 
 /**
  * Takes a mutable list of news items and sorts them by publication date in descending order
+ * since the highest id is the most recent news item
  * @param news the list of news items
  * @return the sorted list
  */
 fun sortNewsByPublicationDate(news: MutableList<NewsItem>): List<NewsItem> {
-    news.sortByDescending { it.publicationDate }
+    news.sortByDescending { it.id}
     return news
 }
